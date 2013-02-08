93858e7a5e8e268a35249a345a2a585386b6adbe
andymcc <andy.mccrae@gmail.com>
2013-02-08 19:32:15 +0000

    Add nTrapy install script

--- ron-cruft/ntrapy.sh
+++ ron-cruft/ntrapy.sh
@@ -5,4 +5,51 @@ set -x
 
 echo INSTALLING AS ${1} against server IP of ${2}
 
-# I feel like this should do more?
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y build-essential git
+
+nvmVersion=0.8.18
+
+function do_git_update() {
+    repo=$1
+
+    if [ -d ${repo} ]; then
+        pushd ${repo}
+        git checkout master
+        git pull origin master
+        popd
+    else
+        git clone git@github.com:rcbops/${repo}
+    fi
+}
+
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+pushd .nvm
+source nvm.sh
+nvm install ${nvmVersion}
+nvm alias default ${nvmVersion}
+popd
+
+do_git_update ntrapy
+
+pushd ntrapy
+cat > config.js <<EOF
+var config = {};
+
+config.roush_url = "http://${2}:8080";
+config.db = "ntrapy";
+config.db_dir = ".";
+config.secret = "???";
+config.timeout = {short: 2000, long: 30000};
+config.interval = 5000;
+
+module.exports = config;
+EOF
+export PATH=$PATH:/root/.nvm/v${nvmVersion}/bin
+
+make
+bash ntrapy
+popd
