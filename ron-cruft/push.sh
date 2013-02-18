22bd92282d4be5f4df23134fbdabd1a9388901d1
andymcc <andy.mccrae@gmail.com>
2013-02-18 12:59:37 -0600

    Updated push script - allows push of nTrapy + specific push/project
    Usage: push.sh <Cluster-Prefix> <repo - e.g. "roush-client">

--- ron-cruft/push.sh
+++ ron-cruft/push.sh
@@ -19,6 +19,9 @@ function on_exit() {
 }
 
 declare -A PIDS
+declare -A IPADDRS
+
+SSHOPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 CLUSTER_PREFIX="c1"
 
@@ -26,9 +29,13 @@ if [ "x$1" != "x" ]; then
     CLUSTER_PREFIX=$1
 fi
 
-# we are assuming that we are sitting in one of the roush
-# directories, and that roush-agent, roush, and roush-client
-# are peers to the directory we are sitting in.
+PUSH_PROJECT="roush-all"
+
+if [ "x$2" != "x" ]; then
+    PUSH_PROJECT=$2
+else
+    echo "No option specified, defaulting to 'roush-all'"
+fi
 
 function mangle_name() {
     server=$1
@@ -51,39 +58,147 @@ function ip_for() {
     fi
 }
 
-if [ ! -d ../roush-agent ] || [ ! -d ../roush ] || [ ! -d ../roush-client ]; then
-    echo 'Not sitting in top level roush dir'
-    exit 1
-fi
+function repo_push() {
+    repo=$1
+    ip=$2
+    echo " - pushing ${repo}"
+    pushd ../${repo} >&99 2>&1
+    git push root@${ip}:/root/${repo} HEAD:master >&99 2>&1
+    ssh ${SSHOPTS} root@${ip} "cd /root/${repo} && git reset --hard" >&99 2>&1
+    popd >&99 2>&1
+}
 
-nodes=$($NOVA list |grep -o "${CLUSTER_PREFIX}-[a-Z0-9_-]*" | grep -v "ntrapy" )
+function push_roush_agent() {
+    if [ ! -d ../roush-agent ] || [ ! -d ../roush ]; then
+        echo "Not sitting in top level roush dir or roush-agent/roush directory doesnt exist"
+        exit 1
+    fi
 
-for node in ${nodes}; do
-    ip=$(ip_for ${node})
+    ip=$1
 
-    echo "Updating ${node}"
+    echo " - killing roush-agent services"
+    ssh ${SSHOPTS} root@${ip} 'if (pgrep -f roush-agen[t]); then pkill -f roush-agen[t]; fi' >&99 2>&1
+    repo="roush-agent"
+    repo_push $repo $ip
+    repo="roush"
+    repo_push $repo $ip
+    echo " - restarting roush-agent"
+    ssh ${SSHOPTS} root@${ip} /bin/bash -c "cd roush-agent; cd roush-agent; PYTHONPATH=../roush screen -S roush-agent -d -m ./roush-agent.py -v -c ./local.conf"
+}
+function push_roush_server() {
+    if [ ! -d ../roush ]; then
+        echo 'Not sitting in top level roush dir or roush directory doesnt exist'
+        exit 1
+    fi
+
+    ip=$1
 
     echo " - killing roush services"
-    ssh root@${ip} 'if (pgrep -f rous[h]); then pkill -f rous[h]; fi' >&99 2>&1
-    echo " - setting git config"
-    ssh root@${ip} 'git config --global receive.denyCurrentBranch ignore'
+    ssh ${SSHOPTS} root@${ip} 'if (pgrep -f roush.p[y]); then pkill -f roush.p[y]; fi' >&99 2>&1
+    repo="roush"
+    repo_push $repo $ip
+    echo " - Restarting roush"
+    ssh ${SSHOPTS} root@${ip} /bin/bash -c 'cd /root/roush; cd /root/roush; screen -S roush-server -d -m ./roush.py -v -c ./local.conf' >&99 2>&1
+}
 
-    for repo in ../roush-agent ../roush-client ../roush; do
-        echo " - pushing $(basename ${repo})"
-        pushd ${repo} >&99 2>&1
-        git push root@${ip}:/root/$(basename ${repo}) HEAD:master >&99 2>&1
-        ssh root@${ip} "cd /root/$(basename ${repo}) && git reset --hard" >&99 2>&1
-        popd >&99 2>&1
-    done
-
-    # ssh root@${ip} /bin/bash -c "cd /root/roush-client; git reset --hard; cd /root/roush; git reset --hard; cd /root/roush-agent; git reset --hard" >&99 2>&1
-    ssh root@${ip} /bin/bash -c 'cd /root/roush-client; cd /root/roush-client; ls; python setup.py install' >&99 2>&1
-
-    echo " - Restarting roush-agent"
-    ssh root@${ip} /bin/bash -c "cd roush-agent; cd roush-agent; PYTHONPATH=../roush screen -S roush-agent -d -m ./roush-agent.py -v -c ./local.conf"
-
-    if [[ ${node} == ${CLUSTER_PREFIX}-roush-server ]]; then
-        echo " - Restarting roush-server"
-        ssh root@${ip} /bin/bash -c 'cd /root/roush; cd /root/roush; screen -S roush-server -d -m ./roush.py -v -c ./local.conf'
+function push_roush_client() {
+    if [ ! -d ../roush-client ]; then
+       echo 'Not sitting in top level roush-client dir or roush-client directory doesnt exist'
+       exit 1
     fi
+
+    ip=$1
+
+    repo="roush-client"
+    repo_push $repo $ip
+    echo " - installing roush-client"
+    ssh ${SSHOPTS} root@${ip} /bin/bash -c 'cd /root/roush-client; cd /root/roush-client; ls; python setup.py install' >&99 2>&1
+}
+
+function push_ntrapy() {
+    if [ ! -d ../ntrapy ]; then
+        echo 'Not sitting in top level ntrapy dir or ntrapy directory doesnt exist'
+        exit 1
+    fi
+
+    ip=$1
+
+    repo="ntrapy"
+    repo_push $repo $ip
+    echo " - restarting ntrapy"
+    ssh ${SSHOPTS} root@${ip} /bin/bash -c 'cd /root/ntrapy; cd /root/ntrapy; ./ntrapy' >&99 2>&1
+}
+
+nodes=$($NOVA list |grep -o "${CLUSTER_PREFIX}-[a-zA-Z0-9_-]*" )
+for node in ${nodes}; do
+    IPADDRS[$node]=$(ip_for ${node})
 done
+
+case "$PUSH_PROJECT" in
+    "roush-all")
+        for node in ${nodes}; do
+            if [ "${node}" != "$(mangle_name 'ntrapy')" ]; then
+                echo "Updating ${node}"
+                echo " - setting git config for ${node} on IP : ${IPADDRS[$node]}"
+                ssh ${SSHOPTS} root@${IPADDRS[$node]} 'git config --global receive.denyCurrentBranch ignore' >&99 2>&1
+                push_roush_client ${IPADDRS[$node]}
+                push_roush_agent ${IPADDRS[$node]}
+                if [ "$node" == "$(mangle_name 'roush-server')" ]; then
+                    push_roush_server ${IPADDRS[$node]}
+                fi
+            fi
+        done
+        ;;
+
+    "roush")
+        for node in ${nodes}; do
+            if [ "$node" != "$(mangle_name 'ntrapy')" ]; then
+                echo "Updating ${node}"
+                echo " - setting git config for ${node} on IP: ${IPADDRS[$node]}"
+                ssh ${SSHOPTS} root@${IPADDRS[$node]} 'git config --global receive.denyCurrentBranch ignore' >&99 2>&1
+                # We have to restart roush_agent after the push.
+                push_roush_agent ${IPADDRS[$node]}
+                if [ "$node" == "$(mangle_name 'roush-server')" ]; then
+                    push_roush_server ${IPADDRS[$node]}
+                fi
+            fi
+        done
+        ;;
+
+    "roush-client")
+        for node in ${nodes}; do
+            if [ "$node" != "$(mangle_name 'ntrapy')" ]; then
+                echo "Updating ${node}"
+                echo " - setting git config for ${node} on IP : ${IPADDRS[$node]}"
+                ssh ${SSHOPTS} root@${IPADDRS[$node]} 'git config --global receive.denyCurrentBranch ignore' >&99 2>&1
+                push_roush_client ${IPADDRS[$node]}
+            fi
+        done
+        ;;
+
+    "roush-agent")
+        for node in ${nodes}; do
+            if [ "$node" != "$(mangle_name 'ntrapy')" ]; then
+                echo "Updating ${node}"
+                echo " - setting git config for ${node} on IP : ${IPADDRS[$node]}"
+                ssh ${SSHOPTS} root@${IPADDRS[$node]} 'git config --global receive.denyCurrentBranch ignore' >&99 2>&1
+                push_roush_agent ${IPADDRS[$node]}
+            fi
+        done
+        ;;
+
+    "ntrapy")
+        for node in ${nodes}; do
+            if [ "$node" == "$(mangle_name 'ntrapy')" ]; then
+                echo "Updating ${node}"
+                echo " - setting git config for ${node} on IP : ${IPADDRS[$node]}"
+                ssh ${SSHOPTS} root@${IPADDRS[$node]} 'git config --global receive.denyCurrentBranch ignore' >&99 2>&1
+                push_ntrapy ${IPADDRS[$node]}
+            fi
+        done
+        ;;
+
+    *)
+        echo "Usage: push.sh <Cluster-Prefix> {roush-all | roush | roush-client | roush-agent | ntrapy}"
+        exit 1
+esac
